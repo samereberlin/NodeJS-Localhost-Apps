b65edf8ee357a0c39c513ed3d725d6f060d5ff03
Samer Eberlin <samer.eberlin@maersk.com>
2022-08-30 11:58:32 +0200

    Modify server implementation to accept query params, and serve static files.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # NodeJS Localhost Apps
 
-NodeJS Localhost Apps project is composed by a set of browser based apps (available via http://localhost:3000) designed to help with daily basis activities (e.g. file manager, text editor, media player, calculator, games, etc.).
+NodeJS Localhost Apps project is a lightweight and modularized application server, composed by a set of browser based utilities (available via http://localhost:3000) designed to help with daily basis activities (e.g. file manager, text editor, media player/miner, calculator, games, etc.).
 
 ## Working in progress
 
-But this is still under development, so please wait for the official release before start using it :-)
+But it is still under development, so please wait for the first official release before start using it :-)
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 global.i18n = require('./.utils/i18n');
+const fs = require('fs');
+const path = require('path');
 
-const apps = require('fs')
+const apps = fs
 	.readdirSync('./', {withFileTypes: true})
 	.filter((app) => app.name[0] !== '.' && app.isDirectory())
 	.map((app) => app.name);
@@ -9,18 +11,31 @@ const html = require('./.templates/html')({title: global.i18n.appList, body: lis
 
 const server = require('http').createServer((req, res) => {
 	try {
-		const indexOfSlash = req.url.indexOf('/', 1);
-		const app = req.url.substring(1, indexOfSlash > 0 ? indexOfSlash : req.url.length);
-		if (apps.includes(app)) {
-			require(`./${app}/index.js`)(req, res);
-		} else {
+		if (req.url === '/') {
 			res.statusCode = 200;
 			res.setHeader('Content-Type', 'text/html');
 			res.end(html);
+		} else if (req.url[1] === '?') {
+			// TODO: implement query parameter processing.
+			res.statusCode = 400;
+			res.end('ERROR 400: Bad Request.');
+		} else {
+			const indexOfSlash = req.url.indexOf('/', 1);
+			const app = req.url.substring(1, indexOfSlash > 0 ? indexOfSlash : req.url.length);
+			if (apps.includes(app)) {
+				require(`./${app}/index.js`)(req, res);
+			} else if (fs.existsSync(path.join(__dirname, '.public', req.url))) {
+				// TODO: Implement static file serving.
+				res.statusCode = 200;
+				res.end('Yes, the file exists.');
+			} else {
+				res.statusCode = 404;
+				res.end('ERROR 404: Not Found.');
+			}
 		}
 	} catch (error) {
 		res.statusCode = 500;
-		res.end('Internal server error.');
+		res.end('ERROR 500: Internal Server Error.');
 	}
 });
 
