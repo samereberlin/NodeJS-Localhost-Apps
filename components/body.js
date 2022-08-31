ffcb69bbbbc48fb9c1be8a99eac0439af4723c24
Samer Eberlin <samer.eberlin@maersk.com>
2022-08-31 19:36:24 +0200

    Remove rounded corners from list-group.

--- components/body.js
+++ components/body.js
@@ -2,7 +2,7 @@ const header = require(`${global.dirname}/components/header`);
 
 module.exports = (params) => `
 		${header({label: params.label, icon: '/favicon.ico'})}
-		<div class="list-group">
+		<div class="list-group rounded-0">
 			${params.apps
 				.map((app) => `<a href="/${app.id}/" class="list-group-item list-group-item-action">${app.name}</a>`)
 				.join('')}
